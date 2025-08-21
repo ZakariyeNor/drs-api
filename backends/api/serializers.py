58a60701f1e998961d22b72067f54e7772576b84
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-08-21 14:04:32 +0000

    Add

--- backends/api/serializers.py
+++ backends/api/serializers.py
@@ -8,9 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = [
             "id", "username", "password",
         ]
-        extra_kwargs = {
-            "password": {"write_only": True}
-        }
+        extra_kwargs = {"password": {"write_only": True}}
     
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
